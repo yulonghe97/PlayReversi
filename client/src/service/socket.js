30fb0c52228c44627abf0f3b2f2b6b0165ae0f44
Yulong He <59072949+yulonghe97@users.noreply.github.com>
2020-12-02 03:22:02 +0800

    changed ip

--- client/src/service/socket.js
+++ client/src/service/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io("http://206.189.90.113", {
   reconnection: true,
   reconnectionDelay: 500,
   reconnectionAttempts: 10,
