af1251dc5935b34c6b953a18875b81621e714cc5
Yulong He <59072949+yulonghe97@users.noreply.github.com>
2020-12-02 12:12:29 +0800

    updated domain

--- client/src/service/socket.js
+++ client/src/service/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("http://206.189.90.113", {
+export const socket = io("https://api.reversi.pro/", {
   reconnection: true,
   reconnectionDelay: 500,
   reconnectionAttempts: 10,
